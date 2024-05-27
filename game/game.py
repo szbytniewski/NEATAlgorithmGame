27a0b9662043bfa3d965d4f7dd6d25f0a4f97e55
szbytniewski <s.zbytniewski.624@studms.ug.edu.pl>
2024-05-27 20:34:54 +0200

    Added all mechanic that allow you to play the game. Also cleaned up the code so that it is easier to find things

--- game/game.py
+++ game/game.py
@@ -1,66 +1,26 @@
 import math
 import time
-
 import pygame
-from imgUtil import blit_rotate_center, scale_image
+from utility import blit_rotate_center
+from assets import load_images
+from car import PlayerCar
 
-GRASS = scale_image(pygame.image.load("C:/Users/zbyta/OneDrive/Pulpit/ReinforcmentTraningInAGame/game/images/Grass.png"), 3)
-TRACK = scale_image(pygame.image.load("C:/Users/zbyta/OneDrive/Pulpit/ReinforcmentTraningInAGame/game/images/Track.png"), 2)
-
-TRACK_BORDER = scale_image(pygame.image.load("C:/Users/zbyta/OneDrive/Pulpit/ReinforcmentTraningInAGame/game/images/outLine.png"), 2)
-
-CAR = scale_image(pygame.image.load("C:/Users/zbyta/OneDrive/Pulpit/ReinforcmentTraningInAGame/game/images/car.png"), 1.5)
+# Load images
+images = load_images()
+GRASS = images["GRASS"]
+TRACK = images["TRACK"]
+TRACK_BORDER = images["TRACK_BORDER"]
+TRACK_BORDER_MASK = images["TRACK_BORDER_MASK"]
+FINISH = images["FINISH"]
+FINISH_MASK = images["FINISH_MASK"]
+FINISH_POSITION = images["FINISH_POSITION"]
+CAR = images["CAR"]
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode([WIDTH,HEIGHT])
 pygame.display.set_caption("Game")
 
 FPS = 120
-
-class Car:
-    def __init__(self, max_vel, rotation_vel):
-        self.img = self.IMG
-        self.max_vel = max_vel
-        self.vel = 0
-        self.rotation_vel = rotation_vel
-        self.angle = 0
-        self.x, self.y = self.START_POS
-        self.acceleration = 0.1
-
-    def rotate(self, left=False, right=False):
-        if left:
-            self.angle += self.rotation_vel
-        elif right:
-            self.angle -= self.rotation_vel
-
-    def draw(self, win):
-        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
-
-    def move_forward(self):
-        self.vel = min(self.vel + self.acceleration, self.max_vel)
-        self.move()
-
-    def move_backward(self):
-        self.vel = max(self.vel - self.acceleration, -self.max_vel/3)
-        self.move()
-
-    def move(self):
-        radians = math.radians(self.angle)
-        vertical = math.cos(radians) * self.vel
-        horizontal = math.sin(radians) * self.vel
-
-        self.y -= vertical
-        self.x -= horizontal
-
-
-class PlayerCar(Car):
-    IMG = CAR
-    START_POS = (200,200)
-    
-    def reduce_speed(self):
-        self.vel = max(self.vel - self.acceleration / 2, 0)
-        self.move()
-
 
 def draw(win, images, player_car):
     for img, pos in images:
@@ -69,22 +29,8 @@ def draw(win, images, player_car):
     player_car.draw(win)
     pygame.display.update()
 
-run=True
-clock = pygame.time.Clock()
-images = [(GRASS,(0,0)), (TRACK,(0,0)), (CAR, (0,0))]
-player_car = PlayerCar(8, 6)
-while run:
-    clock.tick(FPS)
 
-    draw(WIN, images, player_car)
-
-    pygame.display.update()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-
+def move_player(player_car):
     keys = pygame.key.get_pressed()
     moved = False
 
@@ -101,5 +47,34 @@ while run:
 
     if not moved:
         player_car.reduce_speed()
+
+run=True
+clock = pygame.time.Clock()
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)),
+          (FINISH, FINISH_POSITION), (TRACK_BORDER, (0, 0))]
+player_car = PlayerCar(4, 4, CAR)
+while run:
+    clock.tick(FPS)
+
+    draw(WIN, images, player_car)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+    move_player(player_car)
+
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()
+
+    finish_poi_collide = player_car.collide(FINISH_MASK, *FINISH_POSITION)
+    if finish_poi_collide != None:
+        if finish_poi_collide[1] == 0:
+            player_car.bounce()
+        else:
+            player_car.reset()
+            print("finish")
+
 
 pygame.quit()
